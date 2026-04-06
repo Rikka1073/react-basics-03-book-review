6b3d5b89e9ca69fca4dcf7c4af016d6f3d2a848d
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2026-04-06 22:23:03 +0900

    react-hook-formのインストール

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useForm } from "react-hook-form";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("UserId")} />
+
+        <input {...register("Email", { required: true })} />
+        {errors.Email && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
 
 export default App;
